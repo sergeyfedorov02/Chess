fc4d40916f8474a58733d0722234fab131165fa2
сергей <sergey_fedorov200001@mail.ru>
2020-05-21 01:39:33 +0300

    поиск фигур на доске исправлен

--- src/sample/ChessPosition.java
+++ src/sample/ChessPosition.java
@@ -5,100 +5,53 @@ import sample.figures.Figure;
 import sample.figures.FigureKind;
 import sample.figures.Square;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChessPosition implements ChessBoard {
-    private final Figure[] figures = new Figure[64];
-    private int index = 0;
+    private final List<Figure> figures = new ArrayList<>();
 
     public void add(Figure figure) {
-        this.figures[this.index++] = figure;
+        figures.add(figure);
     }
 
     public ChessPosition copy() {
        var newPosition = new ChessPosition();
-        Arrays.stream(this.figures).forEach(item ->
-        {
-            if (item != null) {
-                newPosition.add(item.copy());
-            }
-        });
+       figures.forEach(item -> newPosition.add(item.copy()));
        return newPosition;
     }
 
     public Figure getFigure(Square square) {
-        int index = this.findBy(square);
-
-        if (index == -1) {
-            return null;
-        }
-        return this.figures[index];
+        return figures.stream().filter(item -> item.position() == square).findFirst().orElse(null);
     }
 
     public void removeFigure(Figure figure) {
-        for (int index = 0; index != this.figures.length; index++) {
-            if (this.figures[index] == figure) {
-                this.figures[index] = null;
-                break;
-            }
-        }
+        figures.remove(figure);
     }
 
     public void clearSquare(Square square) {
-        // удаляем фигуру в позиции, если она там есть
-        int index = this.findBy(square);
+        var figure = figures.stream().filter(item -> item.position() == square).findFirst().orElse(null);
 
-        if (index == -1) {
-            return;
+        if (figure != null) {
+            figures.remove(figure);
         }
-
-        this.figures[index] = null;
     }
 
     public Figure getKing(boolean isWhite) {
-        for (int index = 0; index != this.figures.length; index++) {
-            var figure = this.figures[index];
-            if (figure != null && figure.getKind() == FigureKind.King && figure.isWhite() == isWhite) {
-                return figure;
-            }
-        }
-        return null;
+        return figures.stream().filter(item -> item.getKind() == FigureKind.King && item.isWhite() == isWhite)
+                .findFirst().orElse(null);
     }
 
     public List<Figure> getFigures (boolean isWhite) {
-        var result = new ArrayList<Figure>();
-
-        for (var index = 0; index != this.figures.length; index++) {
-            var figure = this.figures[index];
-            if (figure != null && figure.isWhite() == isWhite) {
-                result.add(figure);
-            }
-        }
-        return result;
+        return figures.stream().filter(item -> item.isWhite() == isWhite).collect(Collectors.toList());
     }
 
     public void clean() {
-        for (var position = 0; position != this.figures.length; position++) {
-            this.figures[position] = null;
-        }
-        this.index = 0;
-    }
-
-    private int findBy(Square square) {
-        int result = -1;
-        for (var index = 0; index != this.figures.length; index++) {
-            if (this.figures[index] != null && this.figures[index].position().equals(square)) {
-                result = index;
-                break;
-            }
-        }
-        return result;
+        figures.clear();
     }
 
     @Override
     public String toString() {
-       return Arrays.toString(this.figures);
+       return Arrays.toString(this.figures.toArray());
     }
 }
